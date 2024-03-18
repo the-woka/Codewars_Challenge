8e6e3301b5a9e20443d4679261f1117cb362c912
the-woka <ois.woka23@gmail.com>
2024-03-19 01:45:28 +0700

    day 1

---
+++ 1-evenOdd.js
@@ -0,0 +1,19 @@
+/* 
+Day 1 - Even Or Odd (8 kyu)
+https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+*/
+
+// function evenOrOdd(number) {
+//   if (number % 2 === 0) {
+//     return "Even";
+//   }
+//   return "Odd";
+// }
+
+// function evenOrOdd(number) {
+//   return number % 2 === 0 ? "Even" : "Odd";
+// }
+
+// Other's answer
+
+const evenOrOdd = (number) => number % 2 ? "Odd" : 'Even';
