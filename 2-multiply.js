95e44cd6d6561a2871066ecf6eec341297a7b8ea
the-woka <ois.woka23@gmail.com>
2024-03-19 01:53:31 +0700

    day 1.1

---
+++ 2-multiply.js
@@ -0,0 +1,12 @@
+/* 
+Day 1.1
+https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+*/
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+const multiply = (a, b) => a * b;
+
+// Other's answer
