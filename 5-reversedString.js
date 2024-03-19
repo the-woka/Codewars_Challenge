26410388ff314e7091fc256a1f5a601f2eb271a6
the-woka <ois.woka23@gmail.com>
2024-03-19 13:38:47 +0700

    day 1.5

---
+++ 5-reversedString.js
@@ -0,0 +1,22 @@
+/* 
+Day 1.5 - Reversed String (8 kyu)
+https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+*/
+
+// function solution(str) {
+//   let reversedArr = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversedArr += str[i];
+//   }
+//   return reversedArr;
+// }
+
+// function solution(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// const solution = (str) => str.split("").reverse().join("");
+
+// Other's Answer
+
+const solution = s => [...s].reverse().join('')
