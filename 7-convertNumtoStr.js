5d991859652aa7472a3f3adf1104f07e4dc8d2db
the-woka <ois.woka23@gmail.com>
2024-03-19 14:02:09 +0700

    day 1.7

---
+++ 7-convertNumtoStr.js
@@ -0,0 +1,23 @@
+/*
+Day 1.7 - Convert a Number to a String! (8 kyu)
+https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+*/
+
+// function numberToString(num) {
+//   let str = num.toString();
+//   return str;
+// }
+
+// function numberToString(num) {
+//     return num.toString();
+// }
+
+// const numberToString = (num) => num.toString();
+
+// Other's Answer
+
+// function numberToString(num) {
+//   return "" + num;
+// }
+
+const numberToString = (num) => `${num}`;
