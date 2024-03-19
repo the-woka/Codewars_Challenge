9bdd09d38a029f7b714418721de6b020005ee7b5
the-woka <ois.woka23@gmail.com>
2024-03-19 14:17:11 +0700

    day 1.8

---
+++ 8-oppositeNum.js
@@ -0,0 +1,26 @@
+/*
+Day 1.8 - Opposite number (8 kyu)
+https://codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+*/
+
+// function opposite(number) {
+//   if (number > 0) {
+//     return -number;
+//   }
+//   return number * -1;
+// }
+
+// function opposite(number) {
+//   return number * -1;
+// }
+
+
+// const opposite = (number) => number * -1;
+
+// Other's Answers
+
+// function opposite(number) {
+//   return -number;
+// }
+
+const opposite = number => -number;
