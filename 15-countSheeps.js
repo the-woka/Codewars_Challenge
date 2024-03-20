ba36b2d00d16663d2fabf0bae3ac163d752244fa
the-woka <ois.woka23@gmail.com>
2024-03-20 16:52:33 +0700

    day 2.5

---
+++ 15-countSheeps.js
@@ -0,0 +1,40 @@
+/*
+Day 2.5 - Couting Sheep... (8 kyu)
+https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
+*/
+
+// function countSheeps(sheep) {
+//   let value = 0;
+//   for (let i = 0; i < sheep.length; i++) {
+//     if (sheep[i]) {
+//       value++;
+//     }
+//   }
+//   return value;
+// }
+
+// function countSheeps(sheep) {
+//   const value = sheep.reduce((acc, curr) => (curr ? acc + 1 : acc), 0);
+//   return value;
+// }
+
+// function countSheeps(sheep) {
+//   return sheep.reduce((acc, curr) => (curr ? acc + 1 : acc), 0);
+// }
+
+const countSheeps = (sheep) =>
+  sheep.reduce((acc, curr) => (curr ? acc + 1 : acc), 0);
+
+console.log(
+  countSheeps([
+    undefined,
+    null,
+    false,
+    true,
+    true,
+    false,
+    null,
+    undefined,
+    true,
+  ])
+);
