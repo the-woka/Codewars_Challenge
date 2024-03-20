d8b557388f2398f86b6c8cf4d307b60a8f25e0b7
the-woka <ois.woka23@gmail.com>
2024-03-21 02:08:52 +0700

    day 3.2

---
+++ 22-returnString.js
@@ -0,0 +1,16 @@
+/*
+Day 3.2 - Returning Strings (8 kyu)
+https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+*/
+
+// function greet(name) {
+//   return "Hello, "+ name +" how are you doing today?";
+// }
+
+// const greet = (name) => "Hello "+ name +"how are you doing today?";
+
+// Other's Answers
+
+const greet = (name) => `Hello, ${name} how are you doing today?`;
+
+console.log(greet("David"));
