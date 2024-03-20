ef21b14a7975eeffc951450cc600aa935938ea5a
the-woka <ois.woka23@gmail.com>
2024-03-20 21:18:26 +0700

    day 2.9

---
+++ 19-keepHydrate.js
@@ -0,0 +1,16 @@
+/*
+Day 2.9 - Keep Hydrated! (8 kyu)
+https://www.codewars.com/kata/582cb0224e56e068d800003c/train/javascript
+*/
+
+// function litres(time) {
+//   return Math.floor(time * 0.5);
+// }
+
+// const litres = (time) => Math.floor(time * 0.5);
+
+// Other's Answers
+
+litres = t => ~~(t / 2);
+
+console.log(litres(6.7));
