92defac48602287472d0248fb874f5e8ebaa6f09
the-woka <ois.woka23@gmail.com>
2024-03-20 16:13:18 +0700

    day 2.3

---
+++ 13-removeSpaces.js
@@ -0,0 +1,39 @@
+/*
+Day 2.2 - Remove String Spaces (8 kyu)
+https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
+*/
+
+// function noSpace(x) {
+//   let arr = x.split("");
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == " ") {
+//         arr[i] = "";
+//     }
+//   }
+//   return arr.join("");
+// }
+
+// function noSpace(x) {
+//   const str = x.replace(/ /g, "");
+//   return str;
+// }
+
+// function noSpace(x) {
+//   return x.replace(/ /g, "");
+// }
+
+// const noSpace = (x) => x.replace(/ /g, "");
+
+// Other's Answers
+
+// function noSpace(x){return x.split(' ').join('')}
+
+// const noSpace = (x) => x.split(' ').join('');
+
+// function noSpace(x) {
+//   return x.replaceAll(" ", "");
+// }
+
+const noSpace = (x) => x.replaceAll(" ", "");
+
+console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
