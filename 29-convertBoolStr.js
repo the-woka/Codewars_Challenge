73ea7f1b70046d44ac434c882bbcf9eb8c349396
the-woka <ois.woka23@gmail.com>
2024-03-21 23:27:04 +0700

    day 3.9

---
+++ 29-convertBoolStr.js
@@ -0,0 +1,20 @@
+/*
+Day 3.9 - Convert a Boolean to a String (8 kyu)
+https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+*/
+
+// function booleanToString(b) {
+//   return ''+b;
+// }
+
+// const booleanToString = (b) => "" + b;
+
+// Other's Answers
+
+// function booleanToString(b) {
+//   return `${b}`;
+// }
+
+const booleanToString = (b) => `${b}`;
+
+console.log(booleanToString(true));
