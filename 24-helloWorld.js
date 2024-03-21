586a43d1352e029885983a41a1c5017459757a2f
the-woka <ois.woka23@gmail.com>
2024-03-21 19:55:42 +0700

    day 3.4

---
+++ 24-helloWorld.js
@@ -0,0 +1,12 @@
+/*
+Day 3.4 - Function 1 - hello world (8 kyu)
+https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+*/
+
+// function greet() {
+//   return "hello world!";
+// }
+
+const greet = () => "hello world!"
+
+console.log(greet());
